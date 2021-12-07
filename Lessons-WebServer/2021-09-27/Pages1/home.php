6b6af5b52e1be9482a06b6f2ae3404c7a0a30596
P110136416 <p110136416@student.peterborough.ac.uk>
2021-12-07 21:56:46 +0000

    removed unrelated projects

--- Lessons-WebServer/2021-09-27/Pages1/home.php
+++
@@ -1,9 +0,0 @@
-<?php 
-    echo "<strong><font color='green' size='12'>Hello World</font></strong>";
-
-    echo "<br><br><br><br>";
-
-
-    echo "<a href='table.php' style='font-size:12pt'>TABLE</a><br>";
-    echo "<a href='variables.php' style='font-size:12pt'>VARIABLES</a>";
-?>
